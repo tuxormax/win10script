42542a9a105563b464dfa29db6a862739b70d3bb
Bernardo Sanchez Gutierrez <4030536+tuxormax@users.noreply.github.com>
2020-12-27 23:06:00 -0600

    don't delete search bar
    If delete search bar, there is a problem with main menu, and the users need the search bar, just delete cortana.

--- dependencias/deleteCortana.bat
+++ dependencias/deleteCortana.bat
@@ -1,11 +1,6 @@
-net stop WSearch
-sc config WSearch start= disabled
-taskkill.exe /F /IM SearchUI.exe
 cmd.exe /c takeown /f "%windir%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy" /r /d s && icacls "%windir%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy" /grant Administradores:F /t
-taskkill.exe /F /IM SearchUI.exe
 ren "%windir%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy\SearchUI.exe" "SearchUIC.exe"
 rd /s /q "%windir%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy"
-Reg.exe add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d "0" /f
 Reg.exe add "HKCU\SOFTWARE\Microsoft\Personalization\Settings" /v "AcceptedPrivacyPolicy" /t REG_DWORD /d "0" /f
 Reg.exe add "HKCU\SOFTWARE\Microsoft\InputPersonalization" /v "RestrictImplicitInkCollection" /t REG_DWORD /d "1" /f
 Reg.exe add "HKCU\SOFTWARE\Microsoft\InputPersonalization" /v "RestrictImplicitTextCollection" /t REG_DWORD /d "1" /f
